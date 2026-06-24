3b2e96501c67ce47759ed0ae71d85f0f1c48b3ef
CanDemirbuken <ycandemirbuken@gmail.com>
2026-06-24 17:41:59 +0300

    Add project files.

---
+++ NotikaIdentityEmail/Context/EmailDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NotikaIdentityEmail.Entities;
+
+namespace NotikaIdentityEmail.Context;
+
+public class EmailDbContext : IdentityDbContext<AppUser>
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=ATSTRNB133;Initial Catalog=NotikaEmailDb;User Id=sa;Password=CanATS2025.;Trust Server Certificate=true;");
+    }
+}
---
+++ NotikaIdentityEmail/Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models;
+
+namespace NotikaIdentityEmail.Controllers;
+
+public class RegisterController(UserManager<AppUser> userManager) : Controller
+{
+    [HttpGet]
+    public IActionResult CreateUser()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateUser(RegisterUserViewModel model)
+    {
+        AppUser appUser = new AppUser()
+        {
+            Name = model.Name,
+            Surname = model.Surname,
+            Email = model.Email,
+            UserName = model.Username
+        };
+
+        var result = await userManager.CreateAsync(appUser, model.Password);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+
+        return RedirectToAction("UserLogin", "Login");
+    }
+}
